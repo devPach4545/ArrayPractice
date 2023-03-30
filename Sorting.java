0a07666e6f5f346d3968ea523c4f5932e5ae36d6
Dhaivat Pachchigar <dpachchigar@ucsd.edu>
2023-03-30 11:49:12 -0700

    select sort

--- Sorting.java
+++ Sorting.java
@@ -1,1 +1,27 @@
+public class Sorting {
+    public static void selectionSort(double[] list) {
+        for(int i = 0; i < list.length; i++) {
+            double currentMin = list[i];
+            int currentMinIndex = i;
 
+            for(int j = i+1; j < list.length; j++) {
+                if(currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if(currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+            System.out.println(list[i]);
+        }   
+
+    }
+
+    public static void main(String[] args) {
+        double a[] = {2,7,3,5,1};
+        selectionSort(a);
+    }
+}
